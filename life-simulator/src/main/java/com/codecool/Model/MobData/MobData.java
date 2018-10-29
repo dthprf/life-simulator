abba7d97d1b0f93102378a31eab522dc0cd9028f
saturator22 <f.szczeski@gmail.com>
2018-10-29 15:39:23 +0100

    Implement MobData abstract class

---
+++ life-simulator/src/main/java/com/codecool/Model/MobData/MobData.java
@@ -0,0 +1,72 @@
+package com.codecool.Model.MobData;
+
+import com.codecool.Model.Point;
+
+import java.util.List;
+
+public abstract class MobData {
+
+    private int energy = 100;
+    private Point position;
+    private int health = 3;
+    private int speed = 10;
+    private int damage = 1;
+    private String breed;
+    private List<String> foodList;
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public List<String> getFoodList() {
+        return foodList;
+    }
+
+    public void setFoodList(List<String> foodList) {
+        this.foodList = foodList;
+    }
+}
