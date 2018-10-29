16717b42c3ead6c93ab137670e2d13f4f5614d35
saturator22 <f.szczeski@gmail.com>
2018-10-29 16:11:17 +0100

    Implement HerbivoreBehaviour body

---
+++ life-simulator/src/main/java/com/codecool/Model/MobBehaviour/HerbivoreBehaviour.java
@@ -0,0 +1,15 @@
+package com.codecool.Model.MobBehaviour;
+
+public class HerbivoreBehaviour implements MobBehaviour{
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void reproduce() {
+        
+    }
+
+}
