d9a9947d2adec1e4af2f38052f17f2ae3a85c171
WojTechM <wojciech.m1997@gmail.com>
2018-10-30 16:10:02 +0100

    Update comparator - sort by YX not XY

--- life-simulator/src/main/java/com/codecool/Comparator/PointComparator.java
+++ life-simulator/src/main/java/com/codecool/Comparator/PointComparator.java
@@ -7,13 +7,13 @@ import java.util.Comparator;
 public class PointComparator implements Comparator<Point> {
     @Override
     public int compare(Point o1, Point o2) {
-        int x1 = o1.getX();
-        int x2 = o2.getX();
-        if (x1 != x2) {
-            return x1 - x2;
-        }
         int y1 = o1.getY();
         int y2 = o2.getY();
-        return y1 - y2;
+        if (y1 != y2) {
+            return y1 - y2;
+        }
+        int x1 = o1.getX();
+        int x2 = o2.getX();
+        return x1 - x2;
     }
 }
