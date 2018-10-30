4386469765c6497e12947f8cbe2a014469375906
WojTechM <wojciech.m1997@gmail.com>
2018-10-30 10:46:37 +0100

    Move 'Board' object from ScavengerBehaviour to Scavenger class

--- life-simulator/src/main/java/com/codecool/MobBehaviour/ScavengerBehaviour.java
+++ life-simulator/src/main/java/com/codecool/MobBehaviour/ScavengerBehaviour.java
@@ -1,19 +1,16 @@
 package com.codecool.MobBehaviour;
 
 import com.codecool.Factory.MobFactory;
-import com.codecool.Model.Board;
 import com.codecool.Model.MobData.MobData;
 
 public class ScavengerBehaviour implements MobBehaviour {
 
     private final MobFactory factory;
     private final MobData mobData;
-    private final Board board;
 
-    public ScavengerBehaviour(MobFactory factory, MobData mobData, Board board) {
+    public ScavengerBehaviour(MobFactory factory, MobData mobData) {
         this.factory = factory;
         this.mobData = mobData;
-        this.board = board;
     }
 
     @Override
--- life-simulator/src/main/java/com/codecool/Model/MobData/Scavenger.java
+++ life-simulator/src/main/java/com/codecool/Model/MobData/Scavenger.java
@@ -1,17 +1,11 @@
 package com.codecool.Model.MobData;
 
+import com.codecool.Model.Board;
 import com.codecool.Model.Point;
 
 public class Scavenger extends MobData {
-    public Scavenger(Point position, String breed) {
-        super(position, breed);
-        this.damage += 3;
-        this.speed += 7;
-    }
-
-    public Scavenger(Point position, String breed, int health) {
-        super(position, breed);
-        this.health = health;
+    public Scavenger(Point position, String breed, Board board) {
+        super(position, breed, board);
         this.damage += 3;
         this.speed += 7;
     }
