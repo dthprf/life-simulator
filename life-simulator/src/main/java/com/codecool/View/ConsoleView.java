51fd45a7a5faec1a3006842007deed0a33d49dde
WojTechM <wojciech.m1997@gmail.com>
2018-10-30 16:27:09 +0100

    ConsoleView implements Runnable

--- life-simulator/src/main/java/com/codecool/View/ConsoleView.java
+++ life-simulator/src/main/java/com/codecool/View/ConsoleView.java
@@ -7,9 +7,29 @@ import com.codecool.Model.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConsoleView {
+public class ConsoleView implements Runnable {
+    private final Board board;
+    private final int interval;
 
-    public void printBoard(Board board) {
+    public ConsoleView(Board board, int interval) {
+        this.board = board;
+        this.interval = interval;
+    }
+
+    @Override
+    public void run() {
+        Thread thread = Thread.currentThread();
+        while (!thread.isInterrupted()) {
+            try {
+                Thread.sleep(interval);
+                printBoard();
+            } catch (InterruptedException e) {
+                // Stop display
+            }
+        }
+    }
+
+    public void printBoard() {
         List<Point> points = new ArrayList<>(board.getBoard().keySet());
         points.sort(new PointComparator());
         int lastY = points.get(0).getY();
@@ -27,4 +47,6 @@ public class ConsoleView {
     private String boardSeparator(Board board) {
         return new String(new char[board.getWidth()]).replace("\0", "-");
     }
+
+
 }
