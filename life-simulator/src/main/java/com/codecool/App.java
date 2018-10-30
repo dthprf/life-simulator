916c8f31379de393a17f729fcfa45465923d30ba
WojTechM <wojciech.m1997@gmail.com>
2018-10-30 13:30:53 +0100

    Create sample implemetation of App class

--- life-simulator/src/main/java/com/codecool/App.java
+++ life-simulator/src/main/java/com/codecool/App.java
@@ -1,13 +1,31 @@
 package com.codecool;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.codecool.Constant.MobTypes;
+import com.codecool.Creator.BoardCreator;
+import com.codecool.Exception.UnrecognizedMobBreedException;
+import com.codecool.Factory.MobFactory;
+import com.codecool.Factory.ResourceSpawner;
+import com.codecool.Model.Board;
+
+public class App {
+
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 20;
+    private static final int RESOURCE_INTERVAL = 1000;
+
+    public static void main(String[] args)  {
+        BoardCreator boardCreator = new BoardCreator();
+        Board board = boardCreator.createBoard(WIDTH, HEIGHT);
+        MobFactory mobFactory = new MobFactory(board);
+        try {
+            mobFactory.spawnMob(2, MobTypes.HERBIVORE_MOB);
+            mobFactory.spawnMob(2, MobTypes.PREDATOR_MOB);
+            mobFactory.spawnMob(2, MobTypes.SCAVENGER_MOB);
+        } catch (UnrecognizedMobBreedException e) {
+            e.printStackTrace();
+        }
+
+        Thread resourceSpawner = new ResourceSpawner(RESOURCE_INTERVAL, board);
+        resourceSpawner.start();
     }
 }
