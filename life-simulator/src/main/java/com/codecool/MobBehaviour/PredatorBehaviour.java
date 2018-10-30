d70957214b64a7bd01ef35e7019e7c7100068df1
Bart <bartpetka@gmail.com>
2018-10-31 00:43:33 +0100

    Implement update() method

--- life-simulator/src/main/java/com/codecool/MobBehaviour/PredatorBehaviour.java
+++ life-simulator/src/main/java/com/codecool/MobBehaviour/PredatorBehaviour.java
@@ -2,13 +2,28 @@ package com.codecool.MobBehaviour;
 
 import com.codecool.Exception.UnrecognizedMobBreedException;
 import com.codecool.Factory.MobFactory;
+import com.codecool.Model.ComponentContainer;
 import com.codecool.Model.MobData.MobData;
 import com.codecool.Model.Point;
+import com.codecool.Model.PredatorAction;
+import com.codecool.Model.Resource;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class PredatorBehaviour implements MobBehaviour {
     private final MobFactory factory;
     private final MobData mobData;
     private final String PREDATOR_MOB = "predator";
+    private final int VIEW_DISTANCE = 4;
+    private final String COLLECT_FOOD = "collect";
+    private final String ATTACK_MOB = "attack";
+    private boolean isMoveDone = false;
 
     public PredatorBehaviour(MobFactory factory, MobData mobData) {
         this.factory = factory;
@@ -17,8 +32,32 @@ public class PredatorBehaviour implements MobBehaviour {
 
     @Override
     public void update() {
+        PriorityQueue<PredatorAction> actionsQueue = queueEfficientActions();
 
+        while (!isMoveDone) {
+            PredatorAction action = actionsQueue.poll();
+
+            if (mobData.getEnergy() > 120) {
+                reproduce();
+                isMoveDone = true;
+            }
+
+            if (action == null) {
+                makeUpdateWhenNoFood();
+                isMoveDone = true;
+
+            } else if (action.isActionInstant()) {
+                proceedInstantAction(action);
+
+            } else {
+                moveTowardsTarget(action.getCoordinate());
+                isMoveDone = true;
+            }
+        }
+        isMoveDone = false;
+        return;
     }
+
 
     @Override
     public void reproduce() {
@@ -28,8 +67,11 @@ public class PredatorBehaviour implements MobBehaviour {
         try {
             factory.spawnMob(currentPosition, energyAfterReproduce, PREDATOR_MOB);
             mobData.setEnergy(energyAfterReproduce);
+
         } catch (UnrecognizedMobBreedException e) {
             e.printStackTrace();
         }
     }
+
+
 }
