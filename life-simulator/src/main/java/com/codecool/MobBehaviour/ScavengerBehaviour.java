aed1780337d51abab2e85690ed2ba146f4e00eab
WojTechM <wojciech.m1997@gmail.com>
2018-10-30 11:30:30 +0100

    Implement reproduce method in ScavengerBehaviour class

--- life-simulator/src/main/java/com/codecool/MobBehaviour/ScavengerBehaviour.java
+++ life-simulator/src/main/java/com/codecool/MobBehaviour/ScavengerBehaviour.java
@@ -1,12 +1,15 @@
 package com.codecool.MobBehaviour;
 
+import com.codecool.Exception.UnrecognizedMobBreedException;
 import com.codecool.Factory.MobFactory;
 import com.codecool.Model.MobData.MobData;
+import com.codecool.Model.Point;
 
 public class ScavengerBehaviour implements MobBehaviour {
 
     private final MobFactory factory;
     private final MobData mobData;
+    private final String SCAVENGER_MOB = "scavenger";
 
     public ScavengerBehaviour(MobFactory factory, MobData mobData) {
         this.factory = factory;
@@ -20,6 +23,13 @@ public class ScavengerBehaviour implements MobBehaviour {
 
     @Override
     public void reproduce() {
-
+        Point currentPosition = mobData.getPosition();
+        int energyAfterReproduce = mobData.getEnergy() / 2;
+        try {
+            factory.spawnMob(currentPosition, energyAfterReproduce, SCAVENGER_MOB);
+            mobData.setEnergy(energyAfterReproduce);
+        } catch (UnrecognizedMobBreedException e) {
+            e.printStackTrace();
+        }
     }
 }
