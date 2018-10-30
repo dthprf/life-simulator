525cc67d1769f7dc408c46fb81b93735652324d1
saturator22 <f.szczeski@gmail.com>
2018-10-30 10:35:02 +0100

    Refactor class due to superclass changes

--- life-simulator/src/main/java/com/codecool/Model/MobData/Herbivore.java
+++ life-simulator/src/main/java/com/codecool/Model/MobData/Herbivore.java
@@ -1,22 +1,16 @@
 package com.codecool.Model.MobData;
 
+import com.codecool.Model.Board;
 import com.codecool.Model.Point;
 
-import java.util.List;
 
 public class Herbivore extends MobData{
 
-    public Herbivore(Point position, String breed) {
-        super(position, breed);
+    public Herbivore(Point position, String breed, Board board) {
+        super(position, breed, board);
         this.health += 5;
         this.damage += 2;
         this.speed += 3;
-    }
-
-    public Herbivore(Point position, String breed, int health) {
-        super(position, breed);
-        this.health = health;
-        this.damage += 2;
-        this.speed += 3;
+        this.foodList = new String[]{"water", "herb"};
     }
 }
