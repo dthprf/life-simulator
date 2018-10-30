97c0bd96a9f86c7c4539f0f453768c47e3f22476
Bart <bartpetka@gmail.com>
2018-10-31 00:44:57 +0100

    Implement PredatorAction class

---
+++ life-simulator/src/main/java/com/codecool/Model/PredatorAction.java
@@ -0,0 +1,54 @@
+package com.codecool.Model;
+
+import com.codecool.Model.MobData.MobData;
+
+public class PredatorAction {
+    private static final int ENERGY_LOOSE_RISK_RATIO = 50;
+    private static final String COLLECT_FOOD = "collect";
+    private static final String ATTACK_MOB = "attack";
+    private String actionType;
+    private int distance;
+    private int energyIncome;
+    private Point coordinate;
+
+    public PredatorAction(Resource resource, int distance, Point coordinate) {
+        this.actionType = COLLECT_FOOD;
+        this.distance = distance;
+        this.energyIncome = resource.getEnergy();
+        this.coordinate = coordinate;
+    }
+
+    public PredatorAction(MobData mob, int distance, Point coordinate) {
+        this.actionType = ATTACK_MOB;
+        this.distance = distance;
+        this.energyIncome = mob.getEnergy();
+        this.coordinate = coordinate;
+    }
+
+    public int calculateEnergyIncome() {
+        return energyIncome - (distance * ENERGY_LOOSE_RISK_RATIO);
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Point getCoordinate() {
+        return coordinate;
+    }
+
+    public int getEnergyIncome() {
+        return energyIncome;
+    }
+
+    public boolean isActionInstant() {
+        if (distance <= 1) {
+            return true;
+        }
+        return false;
+    }
+}
