0789207dd19629a0f0e6491b60eac71d38dddf9f
Bart <bartpetka@gmail.com>
2018-10-30 11:06:08 +0100

    Implement Predator class

---
+++ life-simulator/src/main/java/com/codecool/Model/MobData/Predator.java
@@ -0,0 +1,12 @@
+package com.codecool.Model.MobData;
+
+import com.codecool.Model.Board;
+import com.codecool.Model.Point;
+
+public class Predator extends MobData {
+    public Predator(Point position, String breed, Board board) {
+        super(position, breed, board);
+        this.damage += 5;
+        this.speed += 5;
+    }
+}
