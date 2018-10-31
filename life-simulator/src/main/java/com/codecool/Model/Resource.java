e1f1f31ebd3ebf7df7626058ab9867b9801381f3
WojTechM <wojciech.m1997@gmail.com>
2018-10-31 14:30:41 +0100

    Add asText field to Resource

--- life-simulator/src/main/java/com/codecool/Model/Resource.java
+++ life-simulator/src/main/java/com/codecool/Model/Resource.java
@@ -3,10 +3,12 @@ package com.codecool.Model;
 public class Resource {
     private int energy;
     private String name;
+    private String asText;
 
-    public Resource(int energy, String name) {
+    public Resource(int energy, String name, String unicode) {
         this.energy = energy;
         this.name = name.toLowerCase();
+        this.asText = unicode;
     }
 
     public int getEnergy() {
@@ -15,5 +17,9 @@ public class Resource {
 
     public String getName() {
         return name;
+    }
+
+    public String getAsText() {
+        return asText;
     }
 }
